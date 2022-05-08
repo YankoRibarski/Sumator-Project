b8f3f5cf4799b1940967b27bbbf04242c15a43fa
YankoRibarski <104983219+YankoRibarski@users.noreply.github.com>
2022-05-08 23:06:57 +0300

    Add files via upload

---
+++ Sumator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FirstStepsinCoding
+{
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
+
+
+
